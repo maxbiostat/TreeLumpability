9ae1cd4092300d9461360d58f896380435cad0e4
Luiz Max Carvalho <luizepidemiologia@gmail.com>
2022-05-29 19:04:17 -0300

    improved MH code to include all clades

--- study_transition_matrix_MH.r
+++ study_transition_matrix_MH.r
@@ -1,72 +1,105 @@
-compute_all_errors <- function(clade){
-  
-  csize <- length(strsplit(clade, "")[[1]])
-  clade.ind <- clade_index(clade, all.splits)
-  
-  S1.c.pos <- which(complete.clade.matrix[, clade.ind] == 1)
-  S0.c.pos <- setdiff(1:K, S1.c.pos)
-  
-  grid.pos <- subset(expand.grid(S1.c.pos, S1.c.pos),
-                     Var1 < Var2)
-  
-  all.errors <- do.call(rbind,
-                        apply(grid.pos, 1,
-                              function(row){
-                                x.pos <- row[1]
-                                y.pos <- row[2]
-                                sum_x <- sum(trans.mat.MH[x.pos, S0.c.pos])
-                                sum_y <- sum(trans.mat.MH[y.pos, S0.c.pos])
-                                return(data.frame(
-                                  index.x = x.pos,
-                                  index.y = y.pos,
-                                  Nx = neighbourhood.sizes[x.pos],
-                                  Ny = neighbourhood.sizes[y.pos],
-                                  A0cx = A0s.sizes[x.pos],
-                                  A0cy = A0s.sizes[y.pos],
-                                  sum.x = sum_x,
-                                  sum.y = sum_y,
-                                  error = sum_x - sum_y
-                                ))
-                              })
-  )
-  all.errors$ntaxa <- n
-  all.errors$clade_size <- csize
-  all.errors$clade <- clade
-  all.errors <- tibble::tibble(all.errors)
-  return(all.errors)
+get_csize <- function(clade){
+  length(strsplit(clade, "")[[1]])
 }
-
-compute_all_dists <- function(clade){
-  clade.ind <- clade_index(clade, all.splits)
-  S1.c.pos <- which(complete.clade.matrix[, clade.ind] == 1)
-  grid.pos <- subset(expand.grid(S1.c.pos, S1.c.pos),
-                     Var1 < Var2)
-  all.dists <- do.call(rbind,
-                        apply(grid.pos, 1,
-                              function(row){
-                                x.pos <- row[1]
-                                y.pos <- row[2]
-                                return(data.frame(
-                                  index.x = x.pos,
-                                  index.y = y.pos,
-                                  rspr_dist = rspr(
-                                    all.trees[[x.pos]],
-                                    all.trees[[y.pos]]
-                                  )
-                                ))
-                              })
+get_stuff <- function(clade){
+  
+  compute_all_errors <- function(clade){
+    
+    csize <- get_csize(clade)
+    clade.ind <- clade_index(clade, all.splits)
+    
+    S1.c.pos <- which(complete.clade.matrix[, clade.ind] == 1)
+    S0.c.pos <- setdiff(1:K, S1.c.pos)
+    
+    grid.pos <- subset(expand.grid(S1.c.pos, S1.c.pos),
+                       Var1 < Var2)
+    
+    all.errors <- do.call(rbind,
+                          apply(grid.pos, 1,
+                                function(row){
+                                  x.pos <- row[1]
+                                  y.pos <- row[2]
+                                  sum_x <- sum(trans.mat.MH[x.pos, S0.c.pos])
+                                  sum_y <- sum(trans.mat.MH[y.pos, S0.c.pos])
+                                  return(data.frame(
+                                    index.x = x.pos,
+                                    index.y = y.pos,
+                                    Nx = neighbourhood.sizes[x.pos],
+                                    Ny = neighbourhood.sizes[y.pos],
+                                    A0cx = A0s.sizes[x.pos],
+                                    A0cy = A0s.sizes[y.pos],
+                                    sum.x = sum_x,
+                                    sum.y = sum_y,
+                                    error = sum_x - sum_y
+                                  ))
+                                })
+    )
+    all.errors$ntaxa <- n
+    all.errors$clade_size <- csize
+    all.errors$clade <- clade
+    all.errors <- tibble::tibble(all.errors)
+    return(all.errors)
+  }
+  
+  compute_all_dists <- function(clade){
+    clade.ind <- clade_index(clade, all.splits)
+    S1.c.pos <- which(complete.clade.matrix[, clade.ind] == 1)
+    grid.pos <- subset(expand.grid(S1.c.pos, S1.c.pos),
+                       Var1 < Var2)
+    all.dists <- do.call(rbind,
+                         apply(grid.pos, 1,
+                               function(row){
+                                 x.pos <- row[1]
+                                 y.pos <- row[2]
+                                 return(data.frame(
+                                   index.x = x.pos,
+                                   index.y = y.pos,
+                                   rspr_dist = rspr(
+                                     all.trees[[x.pos]],
+                                     all.trees[[y.pos]]
+                                   )
+                                 ))
+                               })
+    )
+    all.dists$ntaxa <- n
+    all.dists$clade <- clade
+    all.dists <- tibble::tibble(all.dists)
+    return(all.dists)
+  }
+  ###
+  
+  clade.nice <- paste0("t", strsplit(clade, "")[[1]])
+  clade.ind <- clade_index(x = clade, all.splits = all.splits)
+  hasclade <- which(complete.clade.matrix[, clade.ind] == 1)
+  parts <- lapply(all.trees[hasclade],
+                  safe_f_c, clade = clade.nice)
+  
+  Nxprimes <- unlist(lapply(parts,
+                            function(x) neighbourhood_size(x$x_prime)$n_size))
+  Nphixcs <- unlist(lapply(parts,
+                           function(x) neighbourhood_size(x$phi_x_c)$n_size))
+  
+  A0s.sizes <- rep(NA, K)
+  A0s.sizes[hasclade] <- neighbourhood.sizes[hasclade] - Nxprimes + Nphixcs
+  
+  all.errors <- compute_all_errors(clade)
+  dists <- compute_all_dists(clade)
+  
+  all.errors.dt <- tibble::tibble(
+    merge(all.errors, dists,
+          by = c("index.x", "index.y",
+                 "ntaxa", "clade") )
   )
-  all.dists$ntaxa <- n
-  all.dists$clade <- clade
-  all.dists <- tibble::tibble(all.dists)
-  return(all.dists)
+  all.errors.dt <- all.errors.dt %>%
+    mutate(bound = A0cx/Nx - (5*A0cy)/(6*Ny))
+  return(all.errors.dt)
 }
 #########################################
 library(PhyloMarkovChains)
 library(phangorn)
 library(tidyverse)
 
-n <- 4
+n <- 6
 all.splits <- sapply(allSplits(n + 1),  paste, collapse = "")
 J <- length(all.splits)
 
@@ -119,47 +152,26 @@ colnames(complete.clade.matrix) <- all.splits
 colMeans(complete.clade.matrix)
 
 ## Computing |A0c|
-clade <- "12"
-clade.nice <- paste0("t", strsplit(clade, "")[[1]])
-clade.ind <- clade_index(x = clade, all.splits = all.splits)
-hasclade <- which(complete.clade.matrix[, clade.ind] == 1)
-parts <- lapply(all.trees[hasclade],
-                safe_f_c, clade = clade.nice)
+clade.sizes <- unlist(lapply(all.splits, get_csize))
+nontrivial <- intersect(which(clade.sizes > 1), which(clade.sizes < n))
+all.ntclades <- all.splits[nontrivial]
+all.errors <- parallel::mclapply(all.ntclades,
+                     function(clade) get_stuff(clade), mc.cores = 8)
 
-Nxprimes <- unlist(lapply(parts,
-                          function(x) neighbourhood_size(x$x_prime)$n_size))
-Nphixcs <- unlist(lapply(parts,
-                         function(x) neighbourhood_size(x$phi_x_c)$n_size))
-
-A0s.sizes <- rep(NA, K)
-A0s.sizes[hasclade] <- neighbourhood.sizes[hasclade] - Nxprimes + Nphixcs
-
-all.errors <- compute_all_errors(clade)
-dists <- compute_all_dists(clade)
-
-with.dists <- tibble::tibble(
-  merge(all.errors, dists,
-        by = c("index.x", "index.y",
-               "ntaxa", "clade") )
-)
-with.dists <- with.dists %>%
-  mutate(bound = A0cx/Nx - (5*A0cy)/(6*Ny))
-with.dists
+all.errors.dt <- do.call(rbind, all.errors)
 
 ## Plotting
-boxplot(error ~ rspr_dist, with.dists)
-boxplot(bound ~ rspr_dist, with.dists)
+boxplot(error ~ rspr_dist, all.errors.dt)
+boxplot(bound ~ rspr_dist, all.errors.dt)
 
-plot(error~bound, with.dists)
-abline(a = 0, b = 1, lwd = 2)
-plot(abs(error)~bound, with.dists)
+plot(error~bound, all.errors.dt)
 abline(a = 0, b = 1, lwd = 2)
 
-range(with.dists$error)
-range(with.dists$bound)
+range(all.errors.dt$error)
+range(all.errors.dt$bound)
 
-with.dists[which.min(with.dists$error), ]
-with.dists[which.max(with.dists$error), ]
+all.errors.dt[which.min(all.errors.dt$error), ]
+all.errors.dt[which.max(all.errors.dt$error), ]
 
-with.dists[which.min(with.dists$bound), ]
-with.dists[which.max(with.dists$bound), ]
+all.errors.dt[which.min(all.errors.dt$bound), ]
+all.errors.dt[which.max(all.errors.dt$bound), ]
